e65a8b95e0ac38c2536816fc13e6a260952fbb83
Dennis Segall <dennis.segall@sbcglobal.net>
2020-04-05 10:11:57 -0500

    initial commit

---
+++ src/main/java/com/dsegall/javaspringpointproject/JavaSpringPointProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dsegall.javaspringpointproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringPointProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringPointProjectApplication.class, args);
+	}
+
+}
